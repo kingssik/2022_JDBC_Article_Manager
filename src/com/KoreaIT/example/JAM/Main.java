0802857a38b979947f71e91c0539ef7f89b70cbd
kingssik <hyunsikkang25@gamil.com>
2022-08-09 18:45:58 +0900

    각기능 내부에서 DB연결을 하지 않고 명령어 입력 직후에 한 번만 연결

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,14 +1,5 @@
 package com.KoreaIT.example.JAM;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 		new App().run();
