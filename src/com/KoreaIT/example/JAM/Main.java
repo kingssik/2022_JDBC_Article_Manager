8f253177c7cd30adcb77a0d684acea322b3d9cd6
kingssik <hyunsikkang25@gamil.com>
2022-08-09 16:49:25 +0900

    JDBC SELECT 테스트

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -12,7 +12,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-
 		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
 
@@ -23,16 +22,17 @@ public class Main {
 			if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
-
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
+				articles.add(article);
 
 				Connection conn = null;
 				PreparedStatement pstmt = null;
+
 				try {
 					Class.forName("com.mysql.jdbc.Driver");
 					String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
@@ -43,8 +43,8 @@ public class Main {
 					String sql = "INSERT INTO article";
 					sql += " SET regDate = NOW()";
 					sql += ", updateDate = NOW()";
-					sql += ", title = '" + title + "'";
-					sql += ", `body` = '" + body + "'";
+					sql += ", title = '" + title +"'";
+					sql += ", `body` = '" + body +"'";
 
 					System.out.println(sql);
 
@@ -53,7 +53,6 @@ public class Main {
 					int affectedRows = pstmt.executeUpdate();
 
 					System.out.println("affectedRows : " + affectedRows);
-					lastArticleId++;
 
 				} catch (ClassNotFoundException e) {
 					System.out.println("드라이버 로딩 실패");
@@ -76,6 +75,8 @@ public class Main {
 					}
 				}
 
+				lastArticleId++;
+
 				System.out.println(article);
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
@@ -86,9 +87,11 @@ public class Main {
 				}
 
 				System.out.println("번호  /  제목");
+
 				for (Article article : articles) {
 					System.out.printf("%d  /  %s\n", article.id, article.title);
 				}
+
 			}
 
 			if (cmd.equals("exit")) {
@@ -96,5 +99,6 @@ public class Main {
 				break;
 			}
 		}
+
 	}
 }
