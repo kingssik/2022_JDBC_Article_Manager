27ea2683d45be636d58b5806426665d3171da3f2
kingssik <hyunsikkang25@gamil.com>
2022-08-09 16:18:58 +0900

    게시물 작성시 DB에insert

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,5 +1,9 @@
 package com.KoreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,6 +11,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 
 		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
@@ -18,14 +23,58 @@ public class Main {
 			if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
+
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
-				articles.add(article);
-				lastArticleId++;
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "'";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRows = pstmt.executeUpdate();
+
+					System.out.println("affectedRows : " + affectedRows);
+					lastArticleId++;
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 				System.out.println(article);
 			} else if (cmd.equals("article list")) {
